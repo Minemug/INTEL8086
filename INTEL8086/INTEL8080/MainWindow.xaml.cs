5768af2e6d3e2ec6962b5520859a20c513f856d4
Minemug <34980966+Minemug@users.noreply.github.com>
2020-07-09 15:58:28 +0200

    upload

---
+++ INTEL8086/INTEL8080/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace INTEL8080
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Functions functions = new Functions();
+        void Fill()
+        {
+            AX.Text = "#" + functions.array[0].ToString("x");
+            BX.Text = "#" + functions.array[1].ToString("x");
+            CX.Text = "#" + functions.array[2].ToString("x");
+            DX.Text = "#" + functions.array[3].ToString("x");
+        }
+        void FillOriginal()
+        {
+            AX.Text = "#" + functions.arrayOriginal[0].ToString("x");
+            BX.Text = "#" + functions.arrayOriginal[1].ToString("x");
+            CX.Text = "#" + functions.arrayOriginal[2].ToString("x");
+            DX.Text = "#" + functions.arrayOriginal[3].ToString("x");
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Fill();
+        }
+
+        private void Count_Click(object sender, RoutedEventArgs e)
+        {
+            var CalcType = TypeOfCalc.SelectedIndex;
+            var fromwhat = from.SelectedIndex;
+            var towhat = to.SelectedIndex;    
+            if (CalcType == 0)
+            {
+                functions.ADD(towhat,fromwhat);
+            }
+            if (CalcType == 1)
+            {
+                functions.MOV(towhat, fromwhat);
+            }
+            if (CalcType == 2)
+            {
+                functions.SWAP(towhat, fromwhat);
+            }
+            Fill();
+        }
+
+
+
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            FillOriginal();
+        }
+
+        private void AddNewRegister_Click(object sender, RoutedEventArgs e)
+        {            
+            var number = NewRegister.Text;           
+            var registerid = RegisterCombo.SelectedIndex;            
+            try
+            {
+                ushort x = ushort.Parse(number);
+                functions.CHANGE(registerid, x);
+            }
+            catch (System.OverflowException)
+            {
+                MessageBox.Show("Za duza liczba!");
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Nie wpisuj liter!");
+            }
+            
+            Fill();
+        }
+
+        private void NewRegister_MouseEnter(object sender, MouseEventArgs e)
+        {
+            NewRegister.Text = "";
+        }
+
+
+        private void Grid_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void AX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ INTEL8086/INTEL8080/functions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace INTEL8080
+{
+    public class Functions
+    {
+        public ushort[] arrayOriginal = new ushort[4] { 0xfd35, 0x4967, 0xd192, 0x7b80 };
+        public ushort[] array = new ushort[4] { 0xfd35, 0x4967, 0xd192, 0x7b80 };
+        public void ADD(int to, int from)
+        {
+            array[to] += array[from];
+        }
+
+        public void MOV(int to, int from)
+        {
+            array[to] = array[from];
+        }
+
+        public void SWAP(int to, int from)
+        {
+            ushort temporal = array[to];
+            array[to] = array[from];
+            array[from] = temporal;
+
+        }
+
+        public void CHANGE(int to,ushort number)
+        {
+            array[to] = number;
+        }
+    }
+}
